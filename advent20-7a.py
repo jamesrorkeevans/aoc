8ad62f9ca1a6123b499787afb5bfa8e243fc700a
jamesrorkeevans <75373479+jamesrorkeevans@users.noreply.github.com>
2020-12-07 14:44:55 +0000

    Add files via upload

---
+++ advent20-7a.py
@@ -0,0 +1,37 @@
+with open("advent20-7a-input.txt","r") as f:
+    input = f.read().split('\n')
+    del input[len(input) - 1]
+
+def rules_dict(input):
+    outers = {}
+    for rule in input:
+        inners = {}
+        outer_bag, inner_bags = rule.split(" bags contain ")
+        inner_bags  = inner_bags.split(",")
+        for bag in inner_bags:
+            bag = bag.replace(" bags.","").replace(" bags","").replace(" bag.","").replace(" bag","").strip()
+            inners[bag[2:]] = bag[:1]
+        outers[outer_bag] = inners
+    return outers
+
+print rules_dict(input)
+
+def can_contain(rules_dict, outer_bag, target_bag):
+    if ' other' in rules_dict[outer_bag].keys():
+        return False
+    elif target_bag in rules_dict[outer_bag].keys():
+        return True
+    else:
+        for bag in rules_dict[outer_bag].keys():
+            return any(can_contain(rules_dict, bag, target_bag) for bag in rules_dict[outer_bag].keys())
+
+def part1(rules_dict, target_bag):
+    return sum(can_contain(rules_dict, rule, 'shiny gold') for rule in rules_dict)
+
+def count_bags(rules_dict, outer_bag):
+    if ' other' in rules_dict[outer_bag]:
+        return 0
+    else:
+        return sum((count_bags(rules_dict, bag)+1)*int(rules_dict[outer_bag][bag]) for bag in rules_dict[outer_bag].keys())
+
+print count_bags(rules_dict(input), 'shiny gold')
